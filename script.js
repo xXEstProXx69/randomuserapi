5f155dd1585d8018ea0e9467cb62f5eccae00266
xXEstProXx69 <xxestproxx@gmail.com>
2026-02-19 13:36:52 +0200

    randomuser

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
+let name = document.querySelector("#name");
+let age = document.querySelector("#age");
+let country = document.querySelector("#country");
+let email = document.querySelector("#email");
+let male = document.querySelector("#male");
+let female = document.querySelector("#female")
+let userData;
+
 async function getData() {
-  const response = await fetch("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400");
+  const response = await fetch("https://randomuser.me/api/");
   const data = await response.json();
-  console.log(data);
+
+
+
+userData = data.results[0];
+
+  let gender = userData.gender;
+
+if (gender === "female") {
+  female.checked = true;
+  } else {
+    male.checked = true;
+  }
+
+  name.value = userData.name.first + " " + userData.name.last;
+  console.log(name.value)
+  age.value = userData.dob.age;
+  country.value = userData.location.country;
+  email.value = userData.email;
+  console.log(userData)
 }
 const dgetbtn = document.querySelector("#getdatabtn")
+dgetbtn.addEventListener("click", getData)
+  
 
-dgetbtn.addEventListener("click", function() {
-	getData()
-	console.log("Click")
-})
+  
+
+
