4d1ca4312839e02e06be4cfc25ced3ad860cedb5
Philipp Enger-Tšižikov <philipp.enger-tsizikov@tptlive.ee>
2026-02-20 10:52:25 +0200

    cors fix

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-async function getData() {
-  const response = await fetch("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400");
-  const data = await response.json();
-  console.log(data);
-}
-const dgetbtn = document.querySelector("#getdatabtn")
+let name = document.querySelector("#name");
+let age = document.querySelector("#age");
+let country = document.querySelector("#country");
+let email = document.querySelector("#email");
+let male = document.querySelector("#male");
+let female = document.querySelector("#female");
+let userData;
 
-dgetbtn.addEventListener("click", function() {
-	getData()
-	console.log("Click")
-})
+async function getData() {
+  const response = await fetch(
+    "https://cors-anywhere.herokuapp.com/https://randomuser.me/api/",
+    {
+      headers: {
+        "X-Requested-With": "XMLHttpRequest"
+      }
+    }
+  );
+
+  const data = await response.json();
+
+  userData = data.results[0];
+  console.log(userData);
+
+  let gender = userData.gender;
+
+  if (gender === "female") {
+    female.checked = true;
+  } else {
+    male.checked = true;
+  }
+
+  name.value = userData.name.first + " " + userData.name.last;
+  age.value = userData.dob.age;
+  country.value = userData.location.country;
+  email.value = userData.email;
+
+  console.log(userData);
+}
+
+const dgetbtn = document.querySelector("#getdatabtn");
+dgetbtn.addEventListener("click", getData);
