d67ff59ed525dbf90ddf3e15b1c8b1642a8f89ec
Philipp Enger-Tšižikov <philipp.enger-tsizikov@tptlive.ee>
2026-02-20 11:02:44 +0200

    cors fix

--- script.js
+++ script.js
@@ -3,37 +3,39 @@ let age = document.querySelector("#age");
 let country = document.querySelector("#country");
 let email = document.querySelector("#email");
 let male = document.querySelector("#male");
-let female = document.querySelector("#female")
+let female = document.querySelector("#female");
 let userData;
 
 async function getData() {
-  const response = await fetch("https://cors-anywhere.herokuapp.com/https://randomuser.me/api/");
+  const response = await fetch(
+    "https://cors-anywhere.herokuapp.com/https://randomuser.me/api/",
+    {
+      headers: {
+        "X-Requested-With": "XMLHttpRequest"
+      }
+    }
+  );
+
   const data = await response.json();
 
-
-userData = data.results[0];
-console.log(userData)
-
-
+  userData = data.results[0];
+  console.log(userData);
 
   let gender = userData.gender;
-if (gender === "female") {
-  female.checked = true;
+
+  if (gender === "female") {
+    female.checked = true;
   } else {
     male.checked = true;
   }
 
   name.value = userData.name.first + " " + userData.name.last;
-  console.log(name.value)
   age.value = userData.dob.age;
   country.value = userData.location.country;
   email.value = userData.email;
-  console.log(userData)
+
+  console.log(userData);
 }
-const dgetbtn = document.querySelector("#getdatabtn")
-dgetbtn.addEventListener("click", getData)
-  
 
-  
-
-
+const dgetbtn = document.querySelector("#getdatabtn");
+dgetbtn.addEventListener("click", getData);
